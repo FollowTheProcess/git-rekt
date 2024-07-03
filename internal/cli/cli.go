1ec15459194e34b545ab6e30113904dccc1c971d
Tom Fleet <tomfleet2018@gmail.com>
2024-07-03 21:18:38 +0100

    Hook up the insults

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -3,7 +3,9 @@ package cli
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/FollowTheProcess/git-rekt/internal/insults"
 	"github.com/spf13/cobra"
 )
 
@@ -17,21 +19,42 @@ var (
 
 // Build builds and returns the git-rekt CLI.
 func Build() *cobra.Command {
+	var (
+		hard  bool // If true, insult them really hard
+		force bool // Do something *really* mean 👀
+	)
 	cmd := &cobra.Command{
-		Use:           "git rekt [OPTIONS]",
+		Use:           "git-rekt",
 		Version:       version,
 		Args:          cobra.NoArgs,
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		Short:         "A profoundly unhelpful git extension",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println("Do something!")
+			if hard {
+				// Really give it to them
+				fmt.Println(insults.GetWorse())
+			} else {
+				// Just a normal insult
+				fmt.Println(insults.Get())
+			}
+
+			if force {
+				if err := os.RemoveAll(".git"); err != nil {
+					return fmt.Errorf("oooof you're lucky, your repo was saved by: %w", err)
+				}
+			}
+
 			return nil
 		},
 	}
 	// Set our custom version and usage templates
 	cmd.SetUsageTemplate(usageTemplate)
 	cmd.SetVersionTemplate(versionTemplate)
+
+	// Set the flags
+	cmd.Flags().BoolVarP(&hard, "hard", "", false, "Do whatever it does... but harder")
+	cmd.Flags().BoolVarP(&force, "force", "f", false, "You won't like this")
 
 	return cmd
 }
